0fa9cf7e17ceeef197453c75db283ce17f2b70f2
Juan Carlos Leon <jleon@nearshoremx.com>
2017-09-06 21:10:03 -0500

    Examen

--- exams/juan/exam1/problem1.cs
+++ exams/juan/exam1/problem1.cs
@@ -1,5 +1,5 @@
 //==========================================================
-// Type your name and student ID here.
+// A01020200
 //==========================================================
 
 using System;
@@ -8,9 +8,36 @@ using System.Text.RegularExpressions;
 
 namespace Exam1 {
     public class Problem1 {
+        public static void delete_comments(string s){
+            Regex fortran = new Regex(
+                @"
+                (?<notCom> ^[^c|C|\*] .*)
+                ",
+                RegexOptions.IgnorePatternWhitespace
+                | RegexOptions.Multiline
+            );
+        
+            foreach (Match match in fortran.Matches(s)) {
+                Console.WriteLine($"{match.Captures[0]}");
+            }
+        }
+        
         public static void Main(String[] args) {
             // Your code goes here.
+            if(args.Length != 1){
+                Console.Error.WriteLine("Error!, Please specify the input file.");
+                Environment.Exit(1);
+            } 
+
+            try{
+                var file = args[0];
+                /* Read the content of the file */
+                var lines = File.ReadAllText(file);
+                delete_comments(lines);
+            } catch (FileNotFoundException e) {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
         }
     }
 }
-
--- exams/juan/exam1/problem2.cs
+++ exams/juan/exam1/problem2.cs
@@ -1,5 +1,5 @@
 //==========================================================
-// Type your name and student ID here.
+// A01020200
 //==========================================================
 
 using System;
@@ -8,9 +8,47 @@ using System.Text.RegularExpressions;
 
 namespace Exam1 {
     public class Problem2 {
+        static string hex2bin(Match m){
+            string hex = m.ToString();
+            string bin = "#";
+            hex = hex.Trim(new Char[] {'#', 'x'});
+            hex = hex.ToUpper();
+
+            int tmp = Convert.ToInt32(hex,16);
+
+            bin = bin + tmp.ToString();
+            return bin;
+        }
+
+        public static void replace_hex(string s){
+            Regex hexa = new Regex(
+                @"
+                (?<Hex> [#][x][\S][^;]*)
+                ",
+                RegexOptions.IgnorePatternWhitespace
+                | RegexOptions.Multiline
+            );
+        
+            string bin = hexa.Replace(s, new MatchEvaluator(Problem2.hex2bin));
+            Console.WriteLine($"{bin}");
+        }
+
         public static void Main(String[] args) {
             // Your code goes here.
+            if(args.Length != 1){
+                Console.Error.WriteLine("Error!, Please specify the input file.");
+                Environment.Exit(1);
+            } 
+
+            try{
+                var file = args[0];
+                /* Read the content of the file */
+                var lines = File.ReadAllText(file);
+                replace_hex(lines);
+            } catch (FileNotFoundException e) {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(1);
+            }
         }
     }
 }
-
