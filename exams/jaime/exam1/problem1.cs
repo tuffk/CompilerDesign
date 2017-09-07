7169b9015ce8118437d92ec37adac8547dc98220
Tuffk <tuffkmulhall@gmail.com>
2017-09-06 19:24:51 -0500

    vaselineando

--- exams/jaime/exam1/problem1.cs
+++ exams/jaime/exam1/problem1.cs
@@ -7,22 +7,40 @@ using System.IO;
 using System.Text.RegularExpressions;
 
 namespace Exam1 {
-    public class Problem1 {
-        public static void Main(String[] args) {
-          if (args.Length != 1) {
-                  Console.Error.WriteLine(
-                          "Please specify the name of the input file.");
-                  Environment.Exit(1);
-          }
+public class Problem1 {
+public static void solve(string input){
+        Regex wc = new Regex(
+                @"
+                        (?<good> ^[^c|C|\*] .*)
+                      ",
+                RegexOptions.IgnorePatternWhitespace
+                | RegexOptions.Multiline
+                );
+                System.IO.StreamWriter file = new System.IO.StreamWriter("prog1.f");
+        foreach (Match m in wc.Matches(input)) {
 
-          try {
-                  var inputPath = args[0];
-                  var input = File.ReadAllText(inputPath);
-                  //TODO: solution goes here
-          } catch (FileNotFoundException e) {
-                  Console.Error.WriteLine(e.Message);
-                  Environment.Exit(1);
-          }
+                Console.WriteLine($"{m.Captures[0]}\n");
+                file.WriteLine($"{m.Captures[0]}");
+
+
         }
-    }
+        file.Close();
+}
+public static void Main(String[] args) {
+        if (args.Length != 1) {
+                Console.Error.WriteLine(
+                        "Please specify the name of the input file.");
+                Environment.Exit(1);
+        }
+
+        try {
+                var inputPath = args[0];
+                var input = File.ReadAllText(inputPath);
+                solve(input);
+        } catch (FileNotFoundException e) {
+                Console.Error.WriteLine(e.Message);
+                Environment.Exit(1);
+        }
+}
+}
 }
---
+++ exams/jaime/exam1/prog1.f
@@ -0,0 +1,16 @@
+      program pi
+      integer numrects, i
+      real mid, height, width, area, sum
+      sum = 0
+      write(*, *) 'Number of rectangles:'
+      read(*, *) numrects
+      width = 1.0 / numrects
+      do 42 i = numrects - 1, 0, -1
+          mid = (i + 0.5) * width
+          height = 4.0 / (1.0 + mid ** 2)
+          sum = sum + height
+42    continue
+      area = width * sum
+      write(*, *) 'Computed pi = ', area
+      stop
+      end
