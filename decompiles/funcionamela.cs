21f1058d5ddbb36b10d82e10f744c690ed0a3fca
Tuffk <tuffkmulhall@gmail.com>
2017-11-25 20:48:17 -0600

    asdsada

--- decompiles/funcionamela.cs
+++ decompiles/funcionamela.cs
@@ -4,11 +4,12 @@ public class Test
   public static int miglob;
   public static string miglob2;
 
-  public static void pito(int x)
+  public static double pito(int x)
   {
     var kuz = 0;
     x++;
     miglob2 = $"algo {x}";
+    return 0.5;
   }
     static public void Main()
     {
